e764944d12e7ef735adb32a6c3f5d0f61295469e
Princeton Rose <decoyzpr@gmail.com>
2019-03-24 18:45:35 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -21,7 +21,7 @@ class CreateArtists < ActiveRecord::Migration[5.2]
 end
 
 # db/migrate/02_add_favorite_food_to_artists.rb
- 
+
 class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
   def change
     add_column :artists, :favorite_food, :string
